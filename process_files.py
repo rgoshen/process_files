17d8941d2fce0e2280f2493bdef9fe89b747bd25
Richard Goshen <rick.goshen@gmail.com>
2023-09-11 14:39:31 -0700

    move everything that deals with reading in the files to its own function

--- process_files.py
+++ process_files.py
@@ -1,24 +1,40 @@
 import os
 
 
-def main():
-    # Get the current working directory
-    current_directory = os.getcwd()
+def get_visible_files(directory):
+    """Gets a list of all the visible files in the given directory.
 
-    # List all the files in the current directory
-    files = os.listdir(current_directory)
+    Args:
+        directory (str): The path to the directory.
 
-    # Create a list of all the visible files
+    Returns:
+        list: A list of all the visible files in the directory.
+    """
+
+    # Get the list of files in the directory.
+    files = os.listdir(directory)
+
+    # Create a list of all the visible files.
     visible_files = []
 
-    # Print out the names of all the files
+    # Iterate through the list of files.
     for file in files:
-        # Check if the file is hidden
+        # Check if the file is hidden.
         if not file.startswith("."):
-            # Add the file to the list of visible files
+            # Add the file to the list of visible files.
             visible_files.append(file)
 
-    # Print out the names of all the visible files
+    return visible_files
+
+
+def main():
+    """Prints out the names of all the visible files in the current directory.
+    """
+
+    # Get the list of visible files in the current directory.
+    visible_files = get_visible_files(os.getcwd())
+
+    # Print out the names of all the visible files.
     for file in visible_files:
         print(file)
 
