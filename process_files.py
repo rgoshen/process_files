46fd1908cec21bafc7051d145178981de110b37d
Richard Goshen <rick.goshen@gmail.com>
2023-09-11 14:14:43 -0700

    add print all files in a directory

--- process_files.py
+++ process_files.py
@@ -1,5 +1,16 @@
+import os
+
+
 def main():
-    print("Hello Python!")
+    # Get the current working directory
+    current_directory = os.getcwd()
+
+    # List all the files in the current directory
+    files = os.listdir(current_directory)
+
+    # Print out the names of all the files
+    for file in files:
+        print(file)
 
 
 if __name__ == "__main__":
