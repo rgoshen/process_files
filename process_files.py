f26547f8935d335eb3ae384ae0463677d272fc97
Richard Goshen <rick.goshen@gmail.com>
2023-09-11 14:19:53 -0700

    add print only visible files

--- process_files.py
+++ process_files.py
@@ -8,8 +8,18 @@ def main():
     # List all the files in the current directory
     files = os.listdir(current_directory)
 
+    # Create a list of all the visible files
+    visible_files = []
+
     # Print out the names of all the files
     for file in files:
+        # Check if the file is hidden
+        if not file.startswith("."):
+            # Add the file to the list of visible files
+            visible_files.append(file)
+
+    # Print out the names of all the visible files
+    for file in visible_files:
         print(file)
 
 
