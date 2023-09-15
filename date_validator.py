900c389e5a5b0003d806ca626152aac8efa11c38
Richard Goshen <rick.goshen@gmail.com>
2023-09-15 16:23:23 -0700

    add doc string to validate_date method

--- date_validator.py
+++ date_validator.py
@@ -3,6 +3,22 @@ import datetime
 
 
 def validate_date(date_str):
+    """
+        Validates a date string in 'YYYY-MM-DD' format.
+
+        Args:
+            date_str (str): The date string to validate.
+
+        Returns:
+            str: The valid date string if the input is in 'YYYY-MM-DD' format.
+
+        Raises:
+            argparse.ArgumentTypeError: If the input date string is not in the correct format.
+
+        Example:
+            >>> validate_date("2023-09-11")
+            '2023-09-11'
+        """
     try:
         datetime.datetime.strptime(date_str, '%Y-%m-%d')
         return date_str
